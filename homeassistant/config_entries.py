139ef62873df273054c7b7ad70afa4c85cb5a9ed
G Johansson <goran.johansson@shiftit.se>
2024-02-13 19:44:45 +0000

    Fix support

--- homeassistant/config_entries.py
+++ homeassistant/config_entries.py
@@ -2290,11 +2290,8 @@ async def support_remove_from_device(hass: HomeAssistant, domain: str) -> bool:
 
 async def support_entry_reconfigure(hass: HomeAssistant, domain: str) -> bool:
     """Test if a domain supports reconfigure flow."""
-    return bool(
-        loader.is_component_module_loaded(hass, f"{domain}.config_flow")
-        and (handler := HANDLERS.get(domain))
-        and hasattr(handler, "async_step_reconfigure")
-    )
+    handler = await _async_get_flow_handler(hass, domain, {})
+    return bool(handler and hasattr(handler, "async_step_reconfigure"))
 
 
 async def _load_integration(
